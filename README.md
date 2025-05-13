4ccad5ebc8f80e1d755fdc51fbe602d26b1a0c58
Reymo Je <rubicsoft0@gmail.com>
2025-05-13 22:52:31 +0700

    Potentiometer as delay control Update README

--- README.md
+++ README.md
@@ -2,12 +2,35 @@
 
 **Hardware** : Arduino Uno R3
 
-| **Pins** | **Components** |
+I'm pretty basic at Arduino, so I learn several fundamentals for it.
+
+[**SHOWCASE VIDEO**][showcase]
+
+## Pins
+
+| **Pin** | **Component** |
 | -- | -- |
-| 3 | Red Light |
+| 2 | Red Light |
 | 4 | Yellow Light |
-| 5 | Green Light |
-| 7 | Buzzer |
-| 12 | Toggle Button |
+| 7 | Green Light |
+| 9 | Buzzer |
+| A1 | Potentiometer |
+
+## User Config
+
+* **MIN_DELAY** : Minimum value of the traffic timing (in seconds)
+* **MAX_DELAY** : Maximum value of the traffic timing (in seconds)
+* **ENABLE_BUZZER** : Ability to use the buzzer pin
+* **BUZZ_TIME** : Set the buzzer timimg (in seconds)
+
+## Follow Me 😃
+
+X : [@reymo_je][x_link]
+Instagram : [@reymo_je_xefron][ig_link]
 
 Created by *Reymo Je*
+
+
+[ig_link]: https://www.instagram.com/reymo_je_xefron/
+[x_link]: https://x.com/reymo_je/
+[showcase]: https://x.com/reymo_je/status/1922318331098378403?t=M5oGT91u-472fo_bKp1Bag&s=19
--- traffic_light/traffic_light.ino
+++ traffic_light/traffic_light.ino
@@ -1,24 +1,33 @@
 /*
   Traffic Light (Red, Yellow, Green)
-  Code by Reymo Je
+  Created by Reymo Je
 
   PINS
-  3 : red light
+  2 : red light
   4 : yellow light
-  5 : green light
-  7 : buzzer
+  7 : green light
+  9 : buzzer
+  A1 : potentiometer
+
 */
 
 
-#define LIGHT_DELAY 2
-#define BUZZER_PIN 7
+/* USER CONFIG */
+#define MIN_DELAY 0.2
+#define MAX_DELAY 5
+#define ENABLE_BUZZER false
+#define BUZZ_TIME 0.1
 
-int led_pins[3] = {3, 4, 5};
 
+#define BUZZER_PIN 9
+#define POT_PIN A1
 
-// Supporting Functions
 void traffic_light(float delay);
-void yell_buzzer(float buzz_time = 0.2);
+void yell_buzzer(float buzz_time);
+
+
+unsigned int led_pins[3] = {2, 4, 7};
+unsigned long traffic_delay;
 
 
 
@@ -29,31 +38,42 @@ void setup() {
   }
 
   // Set the buzzer pin to output
+  #if ENABLE_BUZZER
   pinMode(BUZZER_PIN, OUTPUT);
+  #endif
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  traffic_light(LIGHT_DELAY);
+  traffic_delay = map(analogRead(POT_PIN), 0, 1023, long(MAX_DELAY * 1000), long(MIN_DELAY * 1000));
+  Serial.println(traffic_delay);
+
+  traffic_light(traffic_delay);
 }
 
 
-/* ------ SUPPORTING FUNCTIONS ------ */
+/* SUPPORTING FUNCTIONS */
 
 // Turn on/off the Red, Yellow, and Green LEDs with delay
-void traffic_light(float delay_time) {
+void traffic_light(unsigned long delay_time) {
   for(int pin : led_pins) {
     digitalWrite(pin, HIGH);
 
-    yell_buzzer();
+    #if ENABLE_BUZZER
+    yell_buzzer(BUZZ_TIME);
+    #endif
 
-    delay(long(delay_time * 1000));
+    delay(delay_time);
     digitalWrite(pin, LOW);
   }
 }
 
+#if ENABLE_BUZZER
 // Turn on a buzzer (pin 7) for a while of time
 void yell_buzzer(float buzz_time) {
   digitalWrite(BUZZER_PIN, HIGH);
   delay(long(buzz_time * 1000));
   digitalWrite(BUZZER_PIN, LOW);
 }
+#endif
